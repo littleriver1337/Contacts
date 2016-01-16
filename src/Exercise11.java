4e4678d4c7bd1d47fee624872102b37ae459ec20
MattBrown <litteriver1337@gmail.com>
2016-01-16 14:03:02 -0500

    re-did exercise 11 for a palidrome!

--- src/Exercise11.java
+++ src/Exercise11.java
@@ -3,30 +3,50 @@ import java.util.Scanner;
 /**
  * Created by MattBrown on 11/11/15.
  */
-public class Exercise11 {
-    public static void main(String[] args) {
-        //Takes a string and gives true or false if it is reversed
-        String word = "";
-        String reversed = "";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your string: ");
-        word = scanner.nextLine();
-        int length = word.length();
-        for (int i = length; i > 0; i--){
-            reversed += word.charAt(i-1);
-        }
-        if (removeSpace(word).compareTo(removeSpace(reversed))== 0;{
-            System.out.println("The String" + word + "is a palindrome");
-        }else{
-            System.out.println("The String" + word + "is not a palindrome");
+class Palindrome {
+
+    public static boolean isPalindrome(String stringToTest) {
+        String workingCopy = removeJunk(stringToTest);
+        String reversedCopy = reverse(workingCopy);
+
+        return reversedCopy.equalsIgnoreCase(workingCopy);
+    }
+
+    protected static String removeJunk(String string) {
+        int i, len = string.length();
+        StringBuffer dest = new StringBuffer(len);
+        char c;
+
+        for (i = (len - 1); i >= 0; i--) {
+            c = string.charAt(i);
+            if (Character.isLetterOrDigit(c)) {
+                dest.append(c);
+            }
         }
 
+        return dest.toString();
     }
-    public static String removeSpace(String aword){
-        String buffer = "";
-        for(int i = 0; i < aword.length(); i++){
-            if(!(String.valueOf(aword.charAt(i).compareTo(" ")== 0)));
-        };
-        return buffer;
+
+    protected static String reverse(String string) {
+        StringBuffer sb = new StringBuffer(string);
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String string = "James";
+
+        System.out.println();
+        System.out.println("Testing whether the following "
+                + "string is a palindrome:");
+        System.out.println("    " + string);
+        System.out.println();
+
+        if (isPalindrome(string)) {
+            System.out.println("It IS a palindrome!");
+        } else {
+            System.out.println("It is NOT a palindrome!");
+        }
+        System.out.println();
     }
 }
